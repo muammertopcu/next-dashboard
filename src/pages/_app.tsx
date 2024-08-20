697854c8ad932fc3d9c9d34c47c2afa6d9fac7bf
muammertopcu <muammertop@outlook.com>
2024-08-20 16:27:01 +0300

    implement: normalize.css and set global font inter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/normalize.css";
+import {Inter} from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import type {AppProps} from "next/app";
+
+const inter = Inter({subsets: ["latin"]});
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
