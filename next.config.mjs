7e3b1b76b5ed2a750ac50e94c996664540356550
muammertopcu <muammertop@outlook.com>
2024-08-20 16:51:15 +0300

    implemented: redirect '/' to 'dashboard'

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {source: '/', destination: '/dashboard', permanent: true }
+    ]
+  }
 };
 
 export default nextConfig;
